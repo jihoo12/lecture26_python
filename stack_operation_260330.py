ff8abece7c34b13e1be343cf5945040f94e8c9ea
jihoo <jihoop0620@gmail.com>
2026-03-30 11:40:53 +0900

    .

---
+++ stack_operation_260330.py
@@ -0,0 +1,22 @@
+stack = []
+capacity = 5
+def isFull():
+    if len(stack) == capacity:
+        return True
+    else:
+        return False
+def push(data):
+    if isFull():
+        print("stack is full")
+    else:
+        stack.append(data)
+print(f'[[정수형 스택 연산 실습(용량:{capacity})]]')
+
+while True:
+    menu = int(input())
+    if menu == 0:
+        break
+    elif menu == 1:
+        data = int(input())
+        push(data)
+print("[[정수형 스택 연산 실습 종료]]")
