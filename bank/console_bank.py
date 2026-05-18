a52413cdd7bd6084715e931e8febedda73dc9bba
jihoo <jihoop0620@gmail.com>
2026-05-18 10:43:15 +0900

    update

--- bank/console_bank.py
+++ bank/console_bank.py
@@ -44,39 +44,39 @@ def select_menu():
     print('========================================================')
     menu = int(input('>> 메뉴 선택 : '))
     return menu
+if __name__ == '__main__':
+    aservice = AccountService()
 
-aservice = AccountService()
+    print()
+    print('============== jihoo Bank ==============')
+    while True:
+        menu = select_menu()
+        if menu == 0: break
 
-print()
-print('============== Hyejeong Bank ==============')
-while True:
-    menu = select_menu()
-    if menu == 0: break
+        elif menu == 1:
+            # 계좌번호, 계좌주, 잔액 입력을 받아서 계좌 생성
+            account_no = input("> 계좌번호 : ")
+            owner = input("> 계좌주 : ")
+            balance = int(input("> 초기입금액 : "))
 
-    elif menu == 1:
-        # 계좌번호, 계좌주, 잔액 입력을 받아서 계좌 생성
-        account_no = input("> 계좌번호 : ")
-        owner = input("> 계좌주 : ")
-        balance = int(input("> 초기입금액 : "))
+            if aservice.create_account(account_no, owner, balance):
+                print('결과 : 계좌가 생성되었습니다.')
 
-        if aservice.create_account(account_no, owner, balance):
-            print('결과 : 계좌가 생성되었습니다.')
+        elif menu == 2: # 계좌목록
+            account_list = aservice.list_account()
+            print('------------')
+            print(' 계좌 목록')
+            print('------------')
+            for account in account_list:
+                print(account)
 
-    elif menu == 2: # 계좌목록
-        account_list = aservice.list_account()
-        print('------------')
-        print(' 계좌 목록')
-        print('------------')
-        for account in account_list:
-            print(account)
-
-    elif menu == 3: # 입금
-        print('------------')
-        print(' 예금 ')
-        print('------------')
-        account_no = input('> 계좌번호')
-        amount = int(input('> 예금액 : '))
-        aservice.deposit(account_no, amount)
+        elif menu == 3: # 입금
+            print('------------')
+            print(' 예금 ')
+            print('------------')
+            account_no = input('> 계좌번호')
+            amount = int(input('> 예금액 : '))
+            aservice.deposit(account_no, amount)
 
 
-print('======== 이용해 주셔서 감사합니다. ========')
+    print('======== 이용해 주셔서 감사합니다. ========')
---
+++ test/console_cal.py
@@ -0,0 +1,3 @@
+import cal
+a,b = map(int,input().split())
+print(cal.add(a,b))
